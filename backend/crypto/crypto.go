974eea0bbcb8cf4c6347b3f7a1d7036e8dbe4caf
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-11-13 18:11:47 +0100

    add crypto helpers

---
+++ backend/crypto/crypto.go
@@ -0,0 +1,27 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(pwd string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(pwd string, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pwd))
+	return err == nil
+}
+
+func NewSessionToken() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", nil
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
