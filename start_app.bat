54a3684e856cdc37bd26d4fc87880e07a1135a76
OBEREZD <65860022+OBEREZD@users.noreply.github.com>
2025-11-18 21:30:20 +0100

    updated start_backend.bat to start frontend as well

---
+++ start_app.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set loc="%CD%"
+
+echo Running backend...
+if exist .\apps\server\cmd\tourbackend (
+    cd .\apps\server\cmd\tourbackend
+    start cmd /k go run .
+)
+
+cd "%loc%"
+
+echo Running frontend...
+if exist .\apps\web (
+    cd .\apps\web
+    start cmd /k npm run dev
+)
+
+exit /b
