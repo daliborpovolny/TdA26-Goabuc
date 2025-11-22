5cac2b952b9c7e5ad70f743ae981c60c28975a81
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-11-22 14:58:57 +0100

    add start of create course page

---
+++ apps/web/src/routes/edit/courses/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    let course_name = ""
+    let course_description = ""
+
+
+
+    function createCourse() {
+        console.log(course_name, course_description)
+    }
+
+</script>
+
+
+
+<div>
+    <h1>Create new course</h1>
+
+    <form on:submit={createCourse}>
+        <label for="course_name">Name</label><br>
+        <input type="text" bind:value={course_name} name="course_name"><br>
+
+        <label for="course_description">Description</label><br>
+        <input type="text" bind:value={course_description} name="course_description"><br>
+
+        <button type="submit">Create</button>
+    </form>
+
+</div>
