63ee29e01e15e28174bba43e44c2aaa8df4f3fbf
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-12-24 20:04:32 +0100

    format courses

--- apps/web/src/routes/courses/+page.svelte
+++ apps/web/src/routes/courses/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import DataLoader from "$lib/components/DataLoader.svelte";
-	import type { Course } from "$lib/types";
+	import DataLoader from '$lib/components/DataLoader.svelte';
+	import type { Course } from '$lib/types';
 
 	let coursesPromise: Promise<Course[]> = loadCourses();
 
@@ -8,21 +8,20 @@
 		return fetch('/api/courses', {
 			method: 'GET',
 			headers: { 'Content-type': 'application/json' }
-		})
-			.then(async (res) => {
-				if (!res.ok) {
-					try {
-						const err = await res.json();
-					} catch {
-						throw new Error('Failed to get list of courses');
-					}
+		}).then(async (res) => {
+			if (!res.ok) {
+				try {
+					const err = await res.json();
+				} catch {
+					throw new Error('Failed to get list of courses');
 				}
-				return res.json();
-			})
+			}
+			return res.json();
+		});
 	}
 </script>
 
-<br>
+<br />
 
 <div class="ml-5">
 	<h1 class="text-2xl">Courses</h1>
@@ -30,14 +29,13 @@
 
 	<DataLoader promise={coursesPromise}>
 		{#snippet children(courses: Course[])}
-		<ul>
-			{#each courses as course}
-				<a class="font-medium" href="/courses/{course.uuid}"> {course.name} </a>
-				<p>{course.description}</p>
-				<br />
-			{/each}
-		</ul>
-
+			<ul>
+				{#each courses as course}
+					<a class="font-medium" href="/courses/{course.uuid}"> {course.name} </a>
+					<p>{course.description}</p>
+					<br />
+				{/each}
+			</ul>
 		{/snippet}
 	</DataLoader>
 </div>
