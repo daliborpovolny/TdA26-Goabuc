82c5645f096be8132fc791cb90fa2a3894faf6a4
OBEREZD <marefak114@gmail.com>
2026-01-11 22:03:39 +0100

    improved caddy autostart, fixed incorrectly autoformatted path

--- start_caddy.sh
+++ start_caddy.sh
@@ -4,11 +4,43 @@ set -euo pipefail
 echo Running caddy...
 
 path_to_back="$PWD/apps/caddy"
+lsof=$(command -v lsof)
+
+echo "Checking for lsof..."
+if ! [[ $lsof ]]; then
+    (
+        echo "lsof not found"
+        read -r -p "Do you want to install lsof? [y/n] " response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+            (
+                read -p "Enter install command (with sudo) (i am not doing this for every OS): " cmd
+                eval $cmd
+            )
+        else
+            (
+                echo "Cannot continue without lsof, exiting..."
+                exit 1
+            )
+        fi
+    )
+fi
+
+echo "Checking for services on port :80..."
+if [[ $(sudo lsof -i:80) ]]; then
+    (
+        echo "Found service, killing violently..."
+        sudo kill -9 $(sudo lsof -t -i:80)
+    )
+else
+    (
+        echo "No service found, proceeding..."
+    )
+fi
 
 if [[ -d "$path_to_back" ]]; then
     (
         cd "$path_to_back"
-        caddy run
+        sudo caddy run
     )
 fi
 
