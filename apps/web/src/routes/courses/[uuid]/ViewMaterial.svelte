d2ab8ba8ff19c7339d7d102f2076ceac622dee08
Dalibor Povolny <dalibor.povolny@gmail.com>
2026-01-12 08:35:28 +0100

    fix materials not showing

--- apps/web/src/routes/courses/[uuid]/ViewMaterial.svelte
+++ apps/web/src/routes/courses/[uuid]/ViewMaterial.svelte
@@ -3,28 +3,27 @@
 
 	let { material }: { material: Material } = $props();
 
-	let collapsed = $state(true)
+	let collapsed = $state(true);
+	$inspect(material, collapsed);
 </script>
 
-<div>
-	{#if !collapsed}
-		<button type="button" onclick={() => (collapsed = !collapsed)} >{material.name}</button>
+<div class="space-y-4 rounded-lg border border-stone-300 bg-stone-50 p-4">
+	<button type="button" onclick={() => (collapsed = !collapsed)}>{material.name}</button>
 
-		{#if material.type === 'file'}
-			<div>
-				<p>{material.description}</p>
+	{#if !collapsed}
+		<div>
+			<p>{material.description}</p>
+			<br />
+			{#if material.type === 'file'}
 				<a class="border border-black bg-stone-200 p-1" href={material.fileUrl}>View</a>
 				<a
 					class="border border-black bg-stone-200 p-1"
 					download={material.name + material.fileUrl.split('.').pop()}
 					href={material.fileUrl}>Download</a
 				>
-			</div>
-		{:else if material.type === 'url'}
-			<div>
-				<p>{material.description}</p>
+			{:else if material.type === 'url'}
 				<a class="border border-black bg-stone-200 p-1" href={material.url}>View</a>
-			</div>
-		{/if}
+			{/if}
+		</div>
 	{/if}
-	</div>
+</div>
